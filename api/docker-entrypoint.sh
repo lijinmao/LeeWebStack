8826f01644ab2c761ab6eda36cc927dcd2df1711
lijinmao <lijinmao@gmail.com>
2023-05-16 08:32:54 +0800

    1

---
+++ api/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# echo 'Run migration'
+# python3 manage.py makemigrations musics
+# python3 manage.py migrate
+# echo 'Create Super User'
+# python3 manage.py createsuperuser --noinput || echo "Super user already created"
+echo 'Collect Static'
+python3 manage.py collectstatic --noinput
+exec "$@"
